b58e48d0e263a737e12cb19c72f5103b9235a805
Alelin Apps <alelinapps@gmail.com>
2022-05-23 12:25:06 -0400

    Initial Commit

---
+++ ThumbsUp/ThumbsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThumbsUpApp.swift
+//  ThumbsUp
+//
+//  Created by Developer on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ThumbsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
